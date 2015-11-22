9973501d16af1c5bbb9fc31f13c2c4aa463360d5
Brittany See <brittanyjsee@gmail.com>
2015-11-22 21:42:13 +0000

    get line, posts controller, and index.html.erb added

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+LighthouseForum::Application.config.secret_key_base = '497a2393764860ddb7fa142cf749cb1a16e582ed790a74fd5078e1fa2a8a562c36053c736266e3bbef55d3aa8acbfc72047b6df176f43b7d7630f95eea281728'
